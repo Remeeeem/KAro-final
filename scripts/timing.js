82f24692191a29a999fef692969ba47c8978726e
Remeeeem <84537811+Remeeeem@users.noreply.github.com>
2022-06-20 11:34:52 +0200

    Timer durch feste Uhrzeit

--- scripts/timing.js
+++ scripts/timing.js
@@ -1,7 +1,7 @@
 //?Zeiten für Fisch Overlay
 let updateTimer = 60;		//Sekunden, nach denen die Seite neu geladen werden soll
-const startTime = new Date("2022-06-20T03:04:00+02:00");
-const endTime = new Date("2022-06-20T03:05:10+02:00");
+const startTime = new Date("2022-06-20T11:37:00+02:00");
+const endTime = new Date("2022-06-20T11:39:10+02:00");
 
 
 const time = document.getElementById("update");
@@ -37,13 +37,13 @@ const overlay = document.getElementById("lag");
 setInterval(() => {
 	now.setSeconds(now.getSeconds() + 1);
 
-	if (startTime < now && now < endTime) {
+	/* if (startTime < now && now < endTime) {
 		overlay.style.display = "flex";
 		overlay.style.opacity = 1;
 	} else {
 		overlay.style.display = "none";
 		overlay.style.opacity = 0;
-	}
+	} */
 
 	//zum Testen (in Production entfernen)
 	if (updateTimer <= 50 && updateTimer > 40) {
